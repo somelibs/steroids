8f842343c950570326f42979458522981508c92a
Paul REBOH <paul@reboh.net>
2025-01-27 18:47:23 +0100

    Steroids - Mixin methods

--- lib/steroids/extensions/modules/mixin_methods.rb
+++ lib/steroids/extensions/modules/mixin_methods.rb
@@ -1,27 +1,27 @@
 module Steroids
   module Extensions
     module Modules
-      module SingletonMethods
+      module MixinMethods
         def grundclass
           self.singleton_class? ? ObjectSpace.each_object(self).to_a.last : self
         end
 
         private
 
-        def singleton(method_name)
+        def mixin(method_name)
           class_name = grundclass.name.split("::").last rescue nil
           if grundclass.methods.include?(method_name)
             grundclass.define_method(method_name) do |*args|
               self.class.send(method_name, *args)
             end
           elsif klass_name == "ClassMethods"
-            raise ArgumentError.new("Can't use singleton on Module ClassMethods. Use included instead.")
+            raise ArgumentError.new("Can't use mixin on Module ClassMethods. Use included instead.")
           else
-            raise ArgumentError.new("Singleton expects a class method")
+            raise ArgumentError.new("Mixin expects a class method")
           end
         end
 
-        def singleton_alias(alias_name, method_name)
+        def mixin_alias(alias_name, method_name)
           grundclass.define_method(alias_name) do |*args|
             self.class.send(method_name, *args)
           end
@@ -34,4 +34,4 @@ module Steroids
   end
 end
 
-Module.include(Steroids::Extensions::Modules::SingletonMethods)
+Module.include(Steroids::Extensions::Modules::MixinMethods)
